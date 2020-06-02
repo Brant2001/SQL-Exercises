7b7f7bee65135122f97dc58399dc965887ed47f9
Brant <brant.pippin@gmail.com>
2020-06-02 15:49:26 -0500

    Added the Music History Exercise.

---
+++ SQL-Exercise-MusicHistory.sql
@@ -0,0 +1,40 @@
+﻿SELECT
+	Id,
+	Label
+FROM Genre;
+
+SELECT
+	Id,
+	ArtistName,
+	YearEstablished
+FROM Artist ORDER BY(ArtistName);
+
+SELECT
+	song.Id,
+	song.Title,
+	song.ArtistId,
+	artist.ArtistName
+FROM Song song JOIN Artist artist ON song.ArtistId = artist.Id;
+
+SELECT
+	Artist.ArtistName,
+	Genre.Label
+FROM Album album
+LEFT JOIN Artist artist ON artist.Id = album.ArtistId
+LEFT JOIN Genre genre ON genre.Id = album.GenreId
+WHERE Album.GenreId = 7;
+
+SELECT
+	Artist.ArtistName,
+	Genre.Label
+FROM Album album
+LEFT JOIN Artist artist ON artist.Id = album.ArtistId
+LEFT JOIN Genre genre ON genre.Id = album.GenreId
+WHERE Album.GenreId = 2 OR Album.GenreId = 4;
+
+SELECT
+	Song.Id,
+	Album.Title
+FROM Song song
+LEFT JOIN Album album ON album.Id = song.AlbumId
+WHERE Song.AlbumId = 0;
